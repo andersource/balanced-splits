ba1583a0cb87846571415a3d7d41371cb54fb497
andersource <andersourcer@gmail.com>
2020-04-15 17:02:18 +0300

    adding setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="balanced-splits",
+    version="0.1.0",
+    author="andersource",
+    author_email="hi@andersource.dev",
+    description="Balanced splitting utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andersource/balanced-splits",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD-3-Clause License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
